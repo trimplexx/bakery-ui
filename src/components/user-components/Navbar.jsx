fccfaab9d9e97be1452d0f61e028862b82a872ea
trimplexx <lkrawczyk2001@gmail.com>
2023-10-19 18:40:45 +0200

    animation on menu navbar

--- src/components/user-components/Navbar.jsx
+++ src/components/user-components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link, NavLink } from 'react-router-dom';
 import './user-styles/Navbar.css';
 import myImage from '../../graphics/BakeryLogo.png';
 import { FaUser, FaShoppingCart, FaBars } from 'react-icons/fa';
-import Login from './Login'
+import Login from './Login';
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,38 +12,36 @@ export const Navbar = () => {
   return (
     <>
       <nav>
-          <Link to="/">
-            <img src={myImage} alt="a" className="logo" />
-          </Link>
-          <FaBars className="toggle_btn" onClick={() => setMenuOpen(!menuOpen)} />
-          <ul className={menuOpen ? "open" : ""}>
-            <li>
-              <NavLink to="/About">O nas</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Contact">Kontakt</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Products">Produkty</NavLink>
-            </li>
-            </ul>
-            <ul className={menuOpen ? "icons-open" : "icons-list"}>
-            <li>
-              <div onClick={() => setModalOpen('login')} className="icon">
-                <FaUser />
-              </div>
-            </li>
-            <li>
-              <NavLink to="/ShoppingCard" className="icon">
-                <FaShoppingCart />
-              </NavLink>
-            </li>
-            </ul>
+        <Link to="/">
+          <img src={myImage} alt="a" className="logo" />
+        </Link>
+        <FaBars className="toggle_btn" onClick={() => setMenuOpen(!menuOpen)} />
+        <ul className={`menu ${menuOpen ? 'open' : ''}`}>
+          <li>
+            <NavLink to="/About">O nas</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact">Kontakt</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Products">Produkty</NavLink>
+          </li>
+        </ul>
+        <ul className={menuOpen ? 'icons-open' : 'icons-list'}>
+          <li>
+            <div onClick={() => setModalOpen('login')} className="icon">
+              <FaUser />
+            </div>
+          </li>
+          <li>
+            <NavLink to="/ShoppingCard" className="icon">
+              <FaShoppingCart />
+            </NavLink>
+          </li>
+        </ul>
       </nav>
 
-      {modalOpen === 'login' && (
-        <Login isOpen={true} onClose={() => setModalOpen(null)}  />
-      )}
+      {modalOpen === 'login' && <Login isOpen={true} onClose={() => setModalOpen(null)} />}
 
       <Outlet />
     </>
